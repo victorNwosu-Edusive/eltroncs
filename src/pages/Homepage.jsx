8ee64e2ab07726e191b4961ac6a592094d49ef3a
Victor Nwosu <nwosuuvictor@gmail.com>
2025-01-20 22:42:59 +0100

    additions to best sellers

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -11,6 +11,7 @@ import Wearable from '../assets/images/watch.png';
 import BestOne from '../assets/images/best-one.webp';
 import BestTwo from '../assets/images/best-two.jpg';
 import BestThree from '../assets/images/best-three.png';
+import BestFour from '../assets/images/best-four.jpg';
 
 function Homepage() {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -201,12 +202,12 @@ function Homepage() {
 
                 <div className="relative border-slate-50 border-[2px] p-3 rounded-md w-56 md:w-56">
                   <div className="absolute bg-slate-100 rounded-full p-2"><Heart size={15} /></div>
-                  <img src={BestOne} alt="" className="rounded-md" />
+                  <img src={BestFour} alt="" className="rounded-md" />
                   <div className="flex flex-col gap-4 mt-4">
                   <div className="flex flex-row justify-between">
                   <div className="flex flex-col">
-                  <span className="text-sm font-bold">iPhone 16 Teal</span>
-                  <p className="text-xs text-gray-700">128GB 8GB RAM</p>
+                  <span className="text-sm font-bold">Oraimo Power Bank</span>
+                  <p className="text-xs text-gray-700">60,000MAH</p>
                   </div>
                   <span className="font-bold text-red-600">$25.99</span>
                   </div>
